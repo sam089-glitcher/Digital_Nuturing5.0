bc21dbdf30d71c257669a6cae73e56bb0f7ff10f
Saumitra Misra <saumitram97@gmail.com>
2026-07-22 02:42:02 +0530

    Spring Core and Maven Exercise4execution.

---
+++ Deepskilling/WEEK 2_DN5.0/Spring Core and Maven/Exercise-4-Creating-and-Configuring-a-Maven-Project/LibraryManagementex4/src/main/java/com/library/management/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.management;
+
+public class BookRepository {
+
+    public void displayRepository() {
+        System.out.println("Book Repository is working.");
+    }
+}
---
+++ Deepskilling/WEEK 2_DN5.0/Spring Core and Maven/Exercise-4-Creating-and-Configuring-a-Maven-Project/LibraryManagementex4/src/main/java/com/library/management/BookService.java
@@ -0,0 +1,15 @@
+package com.library.management;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayService() {
+        System.out.println("Book Service is working.");
+        bookRepository.displayRepository();
+    }
+}
