286e144b73a97cf11fbdbdf262f66f49fffdd860
Saumitra Misra <saumitram97@gmail.com>
2026-07-22 14:55:01 +0530

    Spring Data JPA Handson-1 execution.

---
+++ Deepskilling/WEEK 2_DN5.0/Spring Data JPA with Hibernate/Hands-on-1-Spring-Data-JPA/Database Ormlearn.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE ormlearn;
+
+USE ormlearn;
+
+USE ormlearn;
+
+CREATE TABLE country(
+    code VARCHAR(2) PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+INSERT INTO country VALUES
+('IN','India'),
+('US','United States of America');
