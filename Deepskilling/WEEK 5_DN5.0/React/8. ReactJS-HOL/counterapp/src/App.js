04e3094ebe524f99cc5d75cf33cf51add089f5b6
Saumitra Misra <saumitram97@gmail.com>
2026-07-24 21:53:53 +0530

    8. ReactJS-HOL done✔️

---
+++ Deepskilling/WEEK 5_DN5.0/React/8. ReactJS-HOL/counterapp/src/App.js
@@ -0,0 +1,12 @@
+import CountPeople from "./CountPeople";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <CountPeople />
+        </div>
+    );
+}
+
+export default App;
